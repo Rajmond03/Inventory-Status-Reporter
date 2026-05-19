b8aa6ffcd0e739298283888bc1c5129383752411
Csokor Rajmond <rajmond@gmail.com>
2026-05-19 18:43:43 +0200

    feat: import csv/json modules and add file paths

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+import json
+
+inventory = {}
+
+INPUT_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\input\Inventory.csv"
+OUTPUT_CSV_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\output\inventory_report.csv"
+OUTPUT_JSON_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\output\critical_products.json"
+
