a7ddc7726280d3322d54cb351d8a3223be1d72bf
Csokor Rajmond <rajmond@gmail.com>
2026-05-19 19:21:40 +0200

    feat: add inventory CSV data to dictionary and fix file paths

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import csv
 import json
+from pathlib import Path
 
 inventory = {}
 
-INPUT_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\input\Inventory.csv"
-OUTPUT_CSV_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\output\inventory_report.csv"
-OUTPUT_JSON_FILE = "C:\Users\Rajmond\Desktop\Programozás\Gyakorlás\In progress\Inventory Status Reporter\output\critical_products.json"
+BASE_DIR = Path(__file__).parent
 
+INPUT_FILE = BASE_DIR / "input" / "inventory.csv"
+OUTPUT_CSV_FILE = BASE_DIR / "output" / "invetory_report.csv"
+OUTPUT_JSON_FILE = BASE_DIR / "output" / "critical_porducts.json"
+
+with open(INPUT_FILE, "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        
+        product_name = row["product_name"],
+        current_stock = float(row["current_stock"])
+        minimum_stock = float(row["minimum_stock"])
+
+        inventory[product_name] = {
+            "current_stock": current_stock,
+            "minimum_stock": minimum_stock
+        }
+     
+print(inventory)
